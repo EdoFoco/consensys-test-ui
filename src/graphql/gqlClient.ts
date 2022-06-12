a21d081856c237b9174e1c01231c20e977536a3b
Edoardo Foco <e.foco@hotmail.it>
2022-06-12 15:43:02 +0200

    cleanup: remove debug code

--- src/graphql/gqlClient.ts
+++ src/graphql/gqlClient.ts
@@ -1,7 +1,5 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-console.log(process.env.REACT_APP_API_URL);
-
 export const gqlClient = new ApolloClient({
   uri: process.env.API_URL,
   cache: new InMemoryCache(),
